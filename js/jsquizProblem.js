1fdac29c7ec3bafbca078db17de1b3e0115d3ac1
yunju <dbswnfnal@naver.com>
2024-06-21 12:30:29 +0900

    feat: 다음버튼 클릭시 다음문제 출력 기능 구현 문제 번호는 증가하고, 타이머는 10초로 초기화 10번 문제이후로는 다음 문제 생성x

--- js/jsquizProblem.js
+++ js/jsquizProblem.js
@@ -4,22 +4,40 @@ let problemCnt = 1;
 let randNumList = [];
 
 $(function () {
+    $('#nextBtn').on('click', function () {
+        console.log("다음문제!!");
+        printNextProblem();
+    });
     timer = setInterval(() => {
-        $('#time').html(`${seconds}초`);
-        if (seconds === 10) {
-            $('#problemCnt').html(`Q) ${problemCnt}/10`);
-            printProblem();
-        }
-        seconds--;
-        if (seconds === 0) {
-            seconds = 10;
-            problemCnt++;
-        }
-        if (problemCnt > 10) {
-            clearInterval(timer);
-        }
+        printProblemEvery10Sec();
     }, 1000);
 });
+
+// 즉시 다음문제 생성 함수
+function printNextProblem() {
+    if (problemCnt < 10) {
+        seconds = 10;
+        problemCnt++;
+        printProblemEvery10Sec();
+    }
+}
+
+// 10초마다 문제 재생성 함수
+function printProblemEvery10Sec() {
+    $('#time').html(`${seconds}초`);
+    if (seconds === 10) {
+        $('#problemCnt').html(`Q) ${problemCnt}/10`);
+        printProblem();
+    }
+    seconds--;
+    if (seconds === 0) {
+        seconds = 10;
+        problemCnt++;
+    }
+    if (problemCnt > 10) {
+        clearInterval(timer);
+    }
+}
 
 // 문제 출력 함수
 function printProblem() {
