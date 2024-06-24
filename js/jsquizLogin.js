68992b5bc31fb8ac0e1bc820dd6bee66e097a06a
yunju <dbswnfnal@naver.com>
2024-06-24 09:52:10 +0900

    refactor: 알림창 기능 Alert함수명으로 분할 리팩토링

--- js/jsquizLogin.js
+++ js/jsquizLogin.js
@@ -1,34 +1,42 @@
 $(function () {
     $("#loginBtn").click(function () {
-        Swal.fire({
-            title: "사용자 이름 확인창",
-            text: `\"${$("#name").val()}\"님으로 하시겠습니까?`,
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: '승인',
-            cancelButtonText: '취소',
-            reverseButtons: true, // 버튼 순서 거꾸로
-            didOpen: () => {
-                $('body').attr("class", "");
-            }
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: '퀴즈 프로그램에 접속합니다.',
-                    text: `${$("#name").val()}님 환영합니다.`,
-                    icon: 'success',
-                    didOpen: () => {
-                        $('body').attr("class", "");
-                    }
-                }
-                ).then(() => {
-                    const user = new User(Date.now(), $("#name").val(), 0);
-                    createUserObj(user);
-                    window.location.href = 'jsquizRank.html';
-                });
-            }
-        })
+        showLoginAlert();
     });
 });
+
+function showLoginAlert() {
+    Swal.fire({
+        title: "사용자 이름 확인창",
+        text: `\"${$("#name").val()}\"님으로 하시겠습니까?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '승인',
+        cancelButtonText: '취소',
+        reverseButtons: true, // 버튼 순서 거꾸로
+        didOpen: () => {
+            $('body').attr("class", "");
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            showConfirmAlert();
+        }
+    });
+}
+
+function showConfirmAlert() {
+    Swal.fire({
+        title: '퀴즈 프로그램에 접속합니다.',
+        text: `${$("#name").val()}님 환영합니다.`,
+        icon: 'success',
+        didOpen: () => {
+            $('body').attr("class", "");
+        }
+    }
+    ).then(() => {
+        const user = new User(Date.now(), $("#name").val(), 0);
+        createUserObj(user);
+        window.location.href = 'jsquizRank.html';
+    });
+}
--- js/jsquizRank.js
+++ js/jsquizRank.js
@@ -1,58 +1,11 @@
 $(function () {
     printRank();
     $("#startBtn").click(function () {
-        let timerInterval;
-        Swal.fire({
-            title: "퀴즈 게임을 시작합니다!",
-            html: "게임 시작 전 <b></b> 초 전",
-            timer: 5000,
-            timerProgressBar: true,
-            didOpen: () => {
-                $('body').attr("class", "");
-                Swal.showLoading();
-                const timer = Swal.getPopup().querySelector("b");
-                timer.textContent = 5;
-                timerInterval = setInterval(() => {
-                    timer.textContent = Math.ceil(Swal.getTimerLeft() / 1000);
-                }, 1000);
-            },
-            willClose: () => {
-                clearInterval(timerInterval);
-            }
-        }).then((result) => {
-            if (result.dismiss === Swal.DismissReason.timer) {
-                window.location.href = 'jsquizProblem.html';
-            }
-        });
+        showStartAlert();
     });
 
     $("#endBtn").click(function () {
-        Swal.fire({
-            title: "Quiz 게임 종료",
-            text: "정말로 종료하시겠습니까?",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: '승인',
-            cancelButtonText: '취소',
-            reverseButtons: true, // 버튼 순서 거꾸로
-            didOpen: () => {
-                $('body').attr("class", "");
-            }
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: '퀴즈 프로그램을 종료합니다.',
-                    text: '로그인 페이지로 이동합니다.',
-                    didOpen: () => {
-                        $('body').attr("class", "");
-                    }
-                }).then(() => {
-                    window.location.href = 'jsquizLogin.html';
-                });
-            }
-        })
+        showEndAlert();
     });
 });
 
@@ -70,4 +23,59 @@ function printRank() {
             </tr>
         `);
     }
+}
+
+function showStartAlert() {
+    let timerInterval;
+    Swal.fire({
+        title: "퀴즈 게임을 시작합니다!",
+        html: "게임 시작 전 <b></b> 초 전",
+        timer: 5000,
+        timerProgressBar: true,
+        didOpen: () => {
+            $('body').attr("class", "");
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timer.textContent = 5;
+            timerInterval = setInterval(() => {
+                timer.textContent = Math.ceil(Swal.getTimerLeft() / 1000);
+            }, 1000);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            window.location.href = 'jsquizProblem.html';
+        }
+    });
+}
+
+function showEndAlert() {
+    Swal.fire({
+        title: "Quiz 게임 종료",
+        text: "정말로 종료하시겠습니까?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '승인',
+        cancelButtonText: '취소',
+        reverseButtons: true, // 버튼 순서 거꾸로
+        didOpen: () => {
+            $('body').attr("class", "");
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: '퀴즈 프로그램을 종료합니다.',
+                text: '로그인 페이지로 이동합니다.',
+                didOpen: () => {
+                    $('body').attr("class", "");
+                }
+            }).then(() => {
+                window.location.href = 'jsquizLogin.html';
+            });
+        }
+    })
 }
